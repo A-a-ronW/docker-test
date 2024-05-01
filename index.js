e9192c3bc2a8f01ad95381bb0fc4b4d5e5ce95df
A-a-ronW <108435631+A-a-ronW@users.noreply.github.com>
2024-05-01 19:58:59 -0400

    Updated listening socket

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var http=require('http');
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/plain'});
     res.end('Hello World\n');
-}).listen(6060,'127.0.0.1');
+}).listen(6060,'0.0.0.0');
+console.log(`Now listening on port 6060`);
