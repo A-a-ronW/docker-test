65b31852259ab06eba346db298bea5ca8efaf1da
A-a-ronW <108435631+A-a-ronW@users.noreply.github.com>
2024-05-01 20:27:39 -0400

    Testing pipeline

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http=require('http');
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('Hello World\n');
+    res.end('Hello World\n published at 5/1/2024');
 }).listen(6060,'0.0.0.0');
 console.log(`Now listening on port 6060`);
