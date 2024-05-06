96f0bb62c33296cd3414ffac4a91d305739af6f2
Aaron White <aaron012301@gmail.com>
2024-05-06 09:59:25 -0400

    testing ssh and pipeline

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http=require('http');
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('Hello World\n published at 5/1/2024');
+    res.end('Hello World\n published at 5/1/2024\n Built on Thinkpad');
 }).listen(6060,'0.0.0.0');
 console.log(`Now listening on port 6060`);
